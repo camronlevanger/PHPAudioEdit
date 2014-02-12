a01da6d632aa941fc24e21f53aace4eafd5d0e95
clevanger <clevanger@insidesales.com>
2014-02-12 15:55:29 -0700

    increased the max file size

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 <form enctype="multipart/form-data" action="upload.php" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+    <input type="hidden" name="MAX_FILE_SIZE" value="10000000000000000000000000" />
     <!-- Name of input element determines name in $_FILES array -->
     Send this file: <input name="userfile" type="file" />
     <input type="submit" value="Upload and Chop..." />
